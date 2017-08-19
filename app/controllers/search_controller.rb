084f7df5635dfe40db63895cde7283176359215d
Stefanni Brasil <stefannibrasil@gmail.com>
2017-08-19 16:30:53 -0300

    mostrando resultados da busca

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -3,8 +3,10 @@ class SearchController < ApplicationController
   end
 
   def create
+    redirect_to search_show_path(donation_local: params[:neighborhood])
   end
-  
+
   def show
+    @donation_locals = DonationLocal.where("neighborhood LIKE ?", "%#{params[:donation_local]}%")
   end
  end
